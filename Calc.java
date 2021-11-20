93fe53d52e41e6d90785f022f7255765b6cadda2
Zeroth <bolakhehemanta777@gmail.com>
2021-11-20 15:52:10 +0545

    calculator added

---
+++ Calc.java
@@ -0,0 +1,15 @@
+
+import static java.lang.System.out;
+
+public class Calc 
+{
+	static int add(int a, int b)
+	{
+		return a + b;
+	}
+
+	public static void main(String[] args)
+	{
+		out.println(add(4, 5));
+	}
+}
